e79747f4c7c31266f7efa85febb4399dde8c8fcf
hussain-adeel <adeel@adeelh.me>
2024-12-19 13:32:56 -0800

    Cotinued Register, Login Logic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
+import sys
 from colors import Color
 
 def main_menu() -> int:
-    while(True):
+    while True:
         print("Menu")
         print("================")
         print("1. Log In")
@@ -11,26 +12,35 @@ def main_menu() -> int:
 
         try:
             if (int(login_screen_select) not in [1, 2, 3]):
-                print('\n' + Color.RED + "Invalid Selection... Please try again." + Color.END + '\n')
-                continue;
-            else:
-                return int(login_screen_select)
-        except:
+                print('\n' + Color.RED + "Invalid Selection... Please try again."
+                + Color.END + '\n')
+                continue
+            return int(login_screen_select)
+        except ValueError:
             print('\n' + Color.RED + "Invalid Input... Please enter a number." + Color.END + '\n')
 
+def log_in():
+    print("Log In")
+    print("================")
+    user_name = input("Enter Username: ")
+    password = input("Enter Password: ")
+
+def register():
+    pass
 
 def main():
     print("Welcome to the Simple Bank app")
 
     main_menu_option = main_menu()
 
-    if (main_menu_option == 1):
+    if main_menu_option == 1:
         print("Logging in...")
-    elif (main_menu_option == 2):
+        log_in()
+    elif main_menu_option == 2:
         print("Register...")
+        register()
     else:
         print("\nHave a good day!\n")
-        exit()
-
+        sys.exit()
 
 main()
